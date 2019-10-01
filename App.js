a87f7c79aef749638fd586e012ce6a9176410afa
Srisanmathi <sanmathi3896@gmail.com>
2019-09-30 23:41:47 -0400

    First connection to the API

--- App.js
+++ App.js
@@ -1,114 +1,110 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Text, StyleSheet, FlatList } from 'react-native'
+import { Card } from 'react-native-elements'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export default class App extends Component {
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: 'initial',
+      film: []
+    }
+  }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
+  componentDidMount() {
+
+    this.setState({ loading: 'true' });
+    const apiUrl = 'https://swapi.co/api/films/';
+    this.loadData(apiUrl);
+
+  }
+
+  loadData(apiUrl) {
+
+    let temp = [];
+    fetch(apiUrl)
+
+      .then((r) => r.json())
+
+      .then((json) => {
+
+        for (let i = 0; i < json.count; i++) {
+
+          let film = {
+            title: json.results[i].title,
+            episode_id: json.results[i].episode_id,
+            opening_crawl: json.results[i].opening_crawl,
+            director: json.results[i].director,
+            producer: json.results[i].producer,
+            release_date: json.results[i].release_date
+          }
+
+          temp.push(film);
+        }
+
+        this.setState({
+          film: temp,
+          loading: "false"
+        })
+
+      }).catch((error) => {
+
+        console.log('api call failed');
+
+      });
+
+  }
+
+  render() {
+    if (this.state.loading === 'initial') {
+      console.log('Initializing the DOM');
+      return <Text>Intializing...</Text>;
+
+    }
+
+    if (this.state.loading === 'true') {
+      console.log('Loading data from the API');
+      return <Text style={styles.loading}>Loading...</Text>;
+    }
+
+    return (
+      
+        <FlatList
+          extraData={this.state}
+          data={this.state.film}
+          renderItem={({ item }) => (
+            <Card
+          title={item.title}
+        >
+          <Text style={styles.entry}>EPISODE ID            :  {item.episode_id}</Text>
+                  <Text style={styles.entry}>DIRECTOR              :  {item.director}</Text>
+                  <Text style={styles.entry}>PRODUCER            :  {item.producer}</Text>
+                  <Text style={styles.entry}>RELEASE DATE     :  {item.release_date}</Text>
+                  <Text style={styles.entry}>{`OPENING CRAWL : 
+
+      ${item.opening_crawl}`}</Text>
+</Card>
+
           )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          numColumns={1}
+          keyExtractor={(item, index) => index.toString()}
+        //creates problem while deleting. Give a unique key then
+        />
+)
+}
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
-export default App;
+  loading: {
+    marginTop: 20,
+    marginLeft: 20,
+    fontSize: 23,
+    fontWeight: "bold"
+  },
+
+  entry: {
+    marginBottom: 5
+  }
+});
